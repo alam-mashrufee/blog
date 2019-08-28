5ed776596745657647f6d4bb450382e038c73cbb
alam-mashrufee <mashrufee@gmail.com>
2019-08-28 16:35:20 +0900

    api app added

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+from .views import CreateView
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = {
+    url(r'^bucketlists/$', CreateView.as_view(), name="create"),
+}
+
+urlpatterns = format_suffix_patterns(urlpatterns)
