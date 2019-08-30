3467c4f805eaa5d3256b8ec504ada04d691ec09b
alam-mashrufee <mashrufee@gmail.com>
2019-08-30 15:16:40 +0900

    api get update delete

--- api/tests.py
+++ api/tests.py
@@ -34,3 +34,30 @@ class ViewTestCase(TestCase):
     def test_api_can_create_a_bucketlist(self):
         #Test the api has bucket creation capability.
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_can_get_a_bucketlist(self):
+        #Test the api can get a bucketlist
+        Bucketlist = Bucketlist.objects.get()
+        response = self.client.get(reverse('details',kwargs={'pk':bucketlist.id}), format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, bucketlist)
+        
+    def test_api_can_update_bucketlist(self):
+        #Test the api can update a given bucketlist.
+        change_bucketlist = {'name':'something new'}
+        response = self.client.put(reverse("details",kwargs={'pk':bucketlist.id}), change_bucketlist, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        
+    def test_api_can_delete_bucketlist(self):
+        #Test the api can delete a bucketlist.
+        Bucketlist = Bucketlist.objects.get()
+        response = self.client.delete(
+         reverse('details', kwargs={'pk':bucketlist.id}),
+                   format='json',
+                   follow=True)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)        
+    
+    
+        
+        
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from .views import CreateView
+from .views import DetailsView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = {
     url(r'^bucketlists/$', CreateView.as_view(), name="create"),
+    url(r'^bucketlists/(?P<pk>[0-9]+)/$', DetailsView.as_view(), name="details")
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
